db3fc76c73e058df365201257b2251ec106704f6
void <void@abyss.com>
2025-01-14 01:30:02 +0300

    initial commit

---
+++ .config/dunst/test_notif.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+notify-send --urgency=low "Low" "This is a low urgency notification"
+notify-send --urgency=normal "Normal" "This is a normal urgency notification"
+notify-send --urgency=critical "Critical" "This is a critical urgency notification"
