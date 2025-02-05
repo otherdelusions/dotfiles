974f13c3a8f528c874245d38bed62e10c86e4cd2
otherdelusions <144830130+otherdelusions@users.noreply.github.com>
2025-02-05 03:00:09 +0300

    changed repo structure to match stow conventions, added small tweaks and changes, cleaned unused stuff

--- bash/.bashrc
+++ bash/.bashrc
@@ -13,7 +13,7 @@ export PATH=$HOME/go/bin:$HOME/.local/bin:$HOME/.cargo/bin:$PATH
 
 export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS' --color=fg:#ffffff,bg:#161616,hl:#08bdba --color=fg+:#f2f4f8,bg+:#262626,hl+:#3ddbd9 --color=info:#78a9ff,prompt:#33b1ff,pointer:#42be65 --color=marker:#ee5396,spinner:#ff7eb6,header:#be95ff'
 
-. "/home/void/.deno/env"
+. "$HOME/.deno/env"
 
 eval "$(starship init bash)"
 
--- install.sh
+++ install.sh
@@ -1,4 +1,23 @@
-echo -e "\033[32mSymlinking .config\033[0m"
-stow --verbose --restow --target=$HOME .
-echo -e "\033[32mSymlinking wallpaper\033[0m"
-mkdir -p ~/Pictures/Wallpaper && stow --verbose --restow --target=$HOME/Pictures/Wallpaper wallpaper
+#!/bin/bash
+
+# Add packages you wish to install here
+# NOTE: assets folder is not meant to be stowed
+# and wallpaper package should be stowed manually using:
+# stow --verbose --adopt --target=[YOUR WALLPAPER FOLDER] wallpaper
+packages=(
+  "alacritty"
+  "bash"
+  "dunst"
+  "helix"
+  "hypr"
+  "imv"
+  "rofi"
+  "starship"
+  "waybar"
+  "yazi"
+)
+
+for package in "${packages[@]}"; do
+    echo -e "\nStowing package: $package"
+    stow --verbose --restow --target="$HOME" "$package"
+done
